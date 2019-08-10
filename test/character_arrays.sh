0f440475870a72893e5c8565b690dc1f6a9f7581
Bjoern Bastian <basbjo@posteo.de>
2019-08-10 12:08:39 +0200

    test: Implement tests for bib-conva, needs fixes

--- test/character_arrays.sh
+++ test/character_arrays.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Generate character arrays for unicode and latex conversion tests.
 
+# LATEX, UNICODE and ASCII are the keywords for the respective array contents.
+
 # MIN_COUNT entries will be filled into MIN_LATEX_ARRAY and MIN_UNICODE_ARRAY.
 # ADD_COUNT entries will be filled into ADD_LATEX_ARRAY and ADD_UNICODE_ARRAY.
-# ALL_COUNT entries will be filled into ALL_LATEX_ARRAY and ALL_UNICODE_ARRAY.
-# MIN_COUNT + ADD_COUNT = ALL_COUNT and ALL_*_ARRAY contain all characters.
+# ALL_COUNT entries will be filled into ALL_LATEX_ARRAY, ALL_UNICODE_ARRAY and
+# ALL_ASCII_ARRAY where MIN_COUNT + ADD_COUNT = ALL_COUNT counts all characters.
 
 # Minimum list of replacements (temporarily escape backslashes).
 MIN_LATEX='
@@ -17,6 +19,12 @@ MIN_UNICODE='
 ä ö ü Ä Ö Ü
 â ê î ô û ŝ Â Ê Î Ô Û
 ç
+&'
+
+MIN_ASCII='
+ae oe ue Ae Oe Ue
+a  e  i  o  u  s  A  E  I  O  U
+c
 &'
 
 # Additional list of replacements (temporarily escape backslashes).
@@ -38,6 +46,14 @@ ADD_UNICODE='
 ã ñ Ã Ñ
 æ œ ß ł Ł ø Ø'
 
+ADD_ASCII='
+i  n
+oe ue Oe Ue
+a  e  A  E
+a  A
+c  r  s  z  C  R  S  Z
+a  n  A  N
+ae oe ss l  L  oe Oe'
 
 # Fill variables into arrays.
 MIN_COUNT=0
@@ -56,6 +72,13 @@ do
   ALL_UNICODE_ARRAY[$MIN_COUNT]=$char
 done
 
+MIN_COUNT=0
+for char in $(tr ' ' '\n' <<< $MIN_ASCII)
+do
+  MIN_COUNT=$(($MIN_COUNT + 1))
+  ALL_ASCII_ARRAY[$MIN_COUNT]=$char
+done
+
 ADD_COUNT=0
 for char in $(tr ' ' '\n' <<< $ADD_LATEX)
 do
@@ -72,4 +95,12 @@ do
   ALL_COUNT=$(($ADD_COUNT + $MIN_COUNT))
   ADD_UNICODE_ARRAY[$ADD_COUNT]=$char
   ALL_UNICODE_ARRAY[$ALL_COUNT]=$char
+done
+
+ADD_COUNT=0
+for char in $(tr ' ' '\n' <<< $ADD_ASCII)
+do
+  ADD_COUNT=$(($ADD_COUNT + 1))
+  ALL_COUNT=$(($ADD_COUNT + $MIN_COUNT))
+  ALL_ASCII_ARRAY[$ALL_COUNT]=$char
 done
